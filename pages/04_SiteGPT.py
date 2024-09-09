9e034f3ff6ae06b9d8db956bc6b858c8803e75ab
Sumin <naenjo0315@naver.com>
2024-09-09 21:56:47 +0900

    #17 SiteGPT

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,24 +1,59 @@
-import asyncio
-from langchain.document_loaders import AsyncChromiumLoader, SitemapLoader
-from langchain.document_transformers import Html2TextTransformer
+from langchain.document_loaders import SitemapLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
 from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks.base import BaseCallbackHandler
 import streamlit as st
+import openai
+import os
 
-asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
+
+    def on_llm_start(self, *args, **kargs):
+        self.message_box = st.empty()
+    
+    def on_llm_end(self, *args, **kargs):
+        save_message(self.message, "ai")
+
+    def on_llm_new_token(self, token, *args, **kargs):
+        self.message += token
+        self.message_box.markdown(self.message)
 
 def parse_page(soup):
     header = soup.find("header")
-    footer = soup.find("footer")
     if header:
-        header.decompose() # decompose() 함수를 사용하면 해당 tag의 요소만 제거할 수 있음.
+        header.decompose()
+    footer = soup.find("footer")
     if footer:
         footer.decompose()
-    return str(soup.get_text()).replace("\n", "").replace("  ", "")
+    for nav in soup.find_all("nav"):
+        if nav:
+            nav.decompose()
+    related_products = soup.find("div", class_="items-start")
+    if related_products:
+        related_products.decompose()
+    more_resources = soup.find("div", class_="astro-zntqmydn")
+    if more_resources:
+        more_resources.decompose()
+    for footer_link in soup.find_all("a", class_="mx-2"):
+        footer_link.decompose()
+    return str(soup.get_text()).replace("\n", " ")
+def save_message(message, role):
+    st.session_state["messages"].append({"message":message, "role":role})
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        save_message(message, role)
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
 
 # function========================================================================
 @st.cache_data(show_spinner="Loading website...")
@@ -29,6 +64,11 @@ def load_website(url):
     )
     loader = SitemapLoader(
         url,
+        filter_urls=[ #입력으로 받을 수 있는 list는 2 종류. 1) data를 load 하고 싶은 url들을 담은 list. 2) 정규식(regular expression)을 사용하는 것.
+            r"^(.*\/ai-gateway\/).*",
+            r"^(.*\/vectorize\/).*",
+            r"^(.*\/workers-ai\/).*",
+        ],
         parsing_function=parse_page, 
     )
     loader.requests_per_second = 1 # 1초당 몇 번 요청할 것인가.
@@ -102,39 +142,57 @@ def choose_answer(inputs):
     condensed = "".join(f"{answer['answer']}\nSource:{answer['source']}\nDate:{answer['date']}\n" for answer in answers)
     return choose_chain.invoke({"question":question, "answers": condensed})
 
-
+def check_api_key(api_key):
+    try:
+        openai.api_key = api_key
+        openai.Model.list()
+        os.environ["OPENAI_API_KEY"] = api_key
+        return True
+    except Exception:
+        st.error("Wrong API Key")
+        return False
 # ==================================================================================
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
 
 st.set_page_config(
     page_title="SiteGPT Home",
     page_icon="🤩"
 )
 st.markdown("""
-# SiteGPT
+# SiteGPT for Cloudflare
 Ask questions about the content of a website.
             
 Start by writing the URL of the website on the sidebar.
 """)
+with st.sidebar:
+    st.write("Github repo:\nhttps://github.com/ddeeen/FullStackGPT")
+    with st.form("api_key"):
+        api_key = st.text_input(label="Enter your OpenAI API key")
+        submit = st.form_submit_button("Submit")
 
 with st.sidebar:
-    url = st.text_input("Write down a  URL", placeholder="ex) example.com")
+    url = st.text_input("Write down a Cloudflare sitemap.xml", placeholder="ex) https://developers.cloudflare.com/sitemap-0.xml")
 
-llm = ChatOpenAI(
-    temperature=0.1,
-    model="gpt-4o-mini",
-)
-
-
-
-if url:
-    if ".xml" not in url:
+if check_api_key(api_key) and url:
+    if ".xml" not in url or "cloudflare" not in url:
         with st.sidebar:
-            st.error("Please write down a Sitemap URL")
+            st.error("Please write down a cloudflare Sitemap URL")
     else:
-        # 2
+        llm = ChatOpenAI(
+            temperature=0.1,
+            model="gpt-4o-mini",
+            callbacks=[
+                ChatCallbackHandler()
+            ]
+        )
         retriever = load_website(url)
-        query = st.text_input("Ask a question to the website.")
-        if query:
+        send_message("I'm ready! Ask away!!", "ai", save=False)
+        paint_history()
+        message = st.chat_input("Ask a question to the website!")
+        if message:
+            send_message(message, "human")
             chain = {"docs": retriever, "question":RunnablePassthrough()} | RunnableLambda(get_answers) | RunnableLambda(choose_answer)
-            result = chain.invoke(query)
-            st.write(result.replace("$", "\$"))
+            with st.chat_message("ai"):
+                chain.invoke(message).content
